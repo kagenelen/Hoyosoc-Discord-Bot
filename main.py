3e5eefa9df7e8dc00c476526a837138520d25438
kagenelen <112757169+kagenelen@users.noreply.github.com>
2023-08-18 19:29:32 +1000

    Update main.py

--- main.py
+++ main.py
@@ -651,7 +651,7 @@ async def leaderboard(interaction):
 		if user == None:
 			continue
 		if r < 10:
-			embed.add_field(name=str(r + 1) + ". " + user.nick,
+			embed.add_field(name=str(r + 1) + ". " + user.display_name,
 							value=str(res[r][1]),
 							inline=False)
 		if user.id == interaction.user.id:
